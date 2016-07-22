51c3efe05d33d38908f300b634cf59d0ac0c62b4
jarindr <jarindr23@hotmail.com>
2016-07-22 11:14:22 +0700

    no message

--- punjaluck/public/index/js/index.js
+++ punjaluck/public/index/js/index.js
@@ -1,4 +1,37 @@
+(function(window,undefined){
+
+  // Bind to StateChange Event
+  History.Adapter.bind(window,'statechange', () => { // Note: We are using statechange instead of popstate
+    const state = History.getState()
+    const target = state.data.target
+
+    switch (target) {
+
+      case 'about':
+      $('#about-page').addClass('active')
+      $('#partner-page').removeClass('active')
+      $('#about').addClass('active')
+      $('#partner').removeClass('active')
+      break
+
+      case 'partner':
+      $('#about-page').removeClass('active')
+      $('#partner-page').addClass('active')
+      $('#partner').addClass('active')
+      $('#about').removeClass('active')
+      break
+
+      default:
+
+    }
+  })
+  // Change our States
+
+})(window)
+
 $(document).ready(()=> {
+  History.Adapter.trigger(window, 'statechange')
+  if(window.location.pathname === '/')  History.pushState({ target: 'about' }, null , `/about`)
   initFooterNavigationHandler()
   initNavbarHandler()
 })
@@ -7,37 +40,21 @@ $(document).ready(()=> {
 function initFooterNavigationHandler () {
   const $footerNavigations = $('.footer__navigation')
   const $main = $('.main')
+
   $(document).on('click','.footer__navigation', (e) => {
     const item = $(e.currentTarget)
-    if (!item.hasClass('active')) {
-      item.addClass('active')
-      $footerNavigations.not(item).removeClass('active')
-    }
+    item.addClass('active')
+    $footerNavigations.not(item).removeClass('active')
+
     const section = item.attr('id')
-    if(section === 'mission') {
-      $('#mission-main').addClass('active')
-      $('#vision-main').removeClass('active')
-    } else {
-      $('#mission-main').removeClass('active')
-      $('#vision-main').addClass('active')
-    }
+    History.pushState({ target: section }, null , `/${section}`)
+
   })
 }
 
 function initNavbarHandler () {
   $(document).on('click','#hamberger-menu', (e) => {
-		$(e.currentTarget).toggleClass('open')
+    $(e.currentTarget).toggleClass('open')
     $('.nav-bar__navigation-container').toggleClass('active')
-	})
-}
-
-function switcher () {
-  const $container = $('.switcher-container')
-  const $elements = $container.find('.switcher-child')
-  `<div class='circle-navigation-container'>
-    <div class='circle-navigation'></div>
-    <div class='circle-navigation'></div>
-    <div class='circle-navigation'></div>
-  </div>`
-
+  })
 }
