f4133207591ba6ed921264051ccb34d7b228e2a2
jarindr <jarindr23@hotmail.com>
2016-07-26 02:12:22 +0700

    slider

--- punjaluck/public/index/js/index.js
+++ punjaluck/public/index/js/index.js
@@ -29,11 +29,14 @@
 
 })(window)
 
-$(document).ready(()=> {
+$(document).ready( () => {
   History.Adapter.trigger(window, 'statechange')
   if(window.location.pathname === '/')  History.pushState({ target: 'about' }, null , `/about`)
+
   initFooterNavigationHandler()
   initNavbarHandler()
+  initSlider('#slider1')
+  initSlider('#slider2')
 })
 
 
@@ -46,7 +49,7 @@ function initFooterNavigationHandler () {
     item.addClass('active')
     $footerNavigations.not(item).removeClass('active')
 
-    const section = item.attr('id')
+     const section = item.attr('id')
     History.pushState({ target: section }, null , `/${section}`)
 
   })
@@ -56,5 +59,30 @@ function initNavbarHandler () {
   $(document).on('click','#hamberger-menu', (e) => {
     $(e.currentTarget).toggleClass('open')
     $('.nav-bar__navigation-container').toggleClass('active')
+  })
+}
+
+function initSlider (sliderId) {
+  $(sliderId).find('.slider-content').each((index,el) => {
+    const id = $(el).attr('id')
+    const triggerClass = `trigger-${id}`
+    const active = index === 0 ? 'active':''
+    const element = `<div class='circle-navigation ${active} ${triggerClass}'></div>`
+    $(document).on('click',`.${triggerClass}`,(e) => {
+      const keyWord = triggerClass.split('-')[1]
+      const contentId = `${keyWord}-page`
+      const button = $(e.currentTarget)
+      $(sliderId).find('.slider-content').each((index,el)=>{
+        const targetId = $(el).attr('id')
+        if(targetId !== contentId){
+          $(`#${targetId}`).removeClass('active')
+        }else{
+          $(`#${targetId}`).addClass('active')
+        }
+      })
+      button.addClass('active')
+      $(sliderId).find('.circle-navigation').not(button).removeClass('active')
+    })
+    $(sliderId).find('.circle-navigation-container').append(element)
   })
 }
